02f8e13c14eced6a5f211d856172a3913601f390
MCRoche <maryclare.roche@gmail.com>
2023-07-17 15:38:14 -0400

    Updated randomize_choice_order to make it faster and more efficient; rows that are not randomized only appear once

--- R/choice_randomization.R
+++ R/choice_randomization.R
@@ -27,6 +27,7 @@
 #' @param freeze_top Optional - Number of rows at the top of the choice list that should stay frozen. Defaults to NULL
 #' @param freeze_bottom Optional - Number of rows at the bottom of the choice list that should stay froze. Defaults to NULL. If both `freeze_top` and `freeze_bottom` are NULL, items with a value less than 90 will be randomized.
 #' @param pth Optional - A file path to where the output should be saved. Defaults to the current working directory
+#' @param formatt Either `"xlsx"` or `"csv"`; Defaults to "xlsx"
 #' @param output Optional - If TRUE will return the xlsx form as a data frame to R. If FALSE the only output is the xlsx file. Defaults to TRUE
 #'
 #' @return A ODK-style choice sheet as a data frame and an .xlsx sheet
@@ -35,6 +36,7 @@ randomize_choice_order <- function(choice_sheet,
                                    freeze_top = NULL,
                                    freeze_bottom = NULL,
                                    pth = NULL,
+                                   formatt = "xlsx",
                                    output = TRUE){
 
   choice_sheet <- readxl::read_xlsx(choice_sheet)
@@ -42,69 +44,72 @@ randomize_choice_order <- function(choice_sheet,
 
   if(is.null(freeze_top) & is.null(freeze_bottom)){
 
-    freeze_rows <- choice_sheet[choice_sheet$name >= 90,]
+    bottom_freeze <- choice_sheet[choice_sheet$name >= 90,]
+    bottom_freeze$RandomChoice <- 0
+
     randomize_rows <- choice_sheet[choice_sheet$name < 90,]
 
   } else if(is.null(freeze_top) & !is.null(freeze_bottom)){
 
-    freeze_rows <- choice_sheet[(n_rows - freeze_bottom + 1):n_rows,]
+    bottom_freeze <- choice_sheet[(n_rows - freeze_bottom + 1):n_rows,]
+    bottom_freeze$RandomChoice <- 0
+
     randomize_rows <- choice_sheet[1:(n_rows + freeze_bottom),]
 
   } else if(!is.null(freeze_top) & is.null(freeze_bottom)){
 
-    freeze_rows <- choice_sheet[1:freeze_top,]
+    top_freeze <- choice_sheet[1:freeze_top,]
+    top_freeze$RandomChoice <- 0
+
     randomize_rows <- choice_sheet[(freeze_top + 1):n_rows,]
 
   } else {
-    top_rows <- choice_sheet[1:freeze_top,]
-    bottom_rows <- choice_sheet[(n_rows - freeze_bottom + 1):n_rows,]
+
+    top_free <- choice_sheet[1:freeze_top,]
+    top_freeze$RandomChoice <- 0
+    bottom_freeze <- choice_sheet[(n_rows - freeze_bottom + 1):n_rows,]
+    bottom_freeze$RandomChoice <- 0
+
     randomize_rows <- choice_sheet[(freeze_top+1):(n_rows - freeze_bottom),]
   }
 
+
+
   all.perm <- combinat::permn(1:nrow(randomize_rows))
 
-  randomized_top <- function(x){
-    new_df <- rbind(randomize_rows[order(all.perm[[x]]),],
-                    freeze_rows)
-    new_df["RandomChoice"] <- x
-    new_df
-  }
+  new_df <- lapply(1:length(all.perm),
+                   (\(x){cbind(randomize_rows[order(all.perm[[x]]),],x)}))
+  new_df <- do.call(rbind,new_df)
+  names(new_df)[5] <- "RandomChoice"
 
-  randomized_bottom <- function(x){
-    new_df <- rbind(freeze_rows,
-                    randomize_rows[order(all.perm[[x]]),])
-    new_df["RandomChoice"] <- x
-    new_df
-  }
 
-  randomized_split <- function(x){
-    new_df <- rbind(top_rows,
-                    randomize_rows[order(all.perm[[x]]),],
-                    bottom_rows)
-    new_df["RandomChoice"] <- x
-    new_df
-  }
 
-  if(!is.null(freeze_top) & !is.null(freeze_bottom)){
+  if(is.null(freeze_top)){
 
-    randomized_choices <- purrr::map_df(1:length(all.perm),
-                                        randomized_split)
+    randomized_choices <- cbind(
+      new_df,
+      bottom_freeze
+    )
 
   } else if(!is.null(freeze_top) & is.null(freeze_bottom)){
 
-    randomized_choices <- purrr::map_df(1:length(all.perm),
-                                        randomized_bottom)
+    randomized_choices <- cbind(
+      top_freeze,
+      new_df
+    )
 
   } else {
 
-    randomized_choices <- purrr::map_df(1:length(all.perm),
-                                        randomized_top)
-  }
+    randomized_choices <- cbind(
+      top_freeze,
+      new_df,
+      bottom_freeze
+    )
 
+  }
 
   insight::format_warning("Do not forget to create a calculate question in the {.b survey} sheet with the following calculation:",
                           stringr::str_glue("once(round((random()*{length(all.perm) - 1})) + 1, 0)"))
-
 
   if(!is.null(pth)){
     if(stringr::str_detect(pth,"/",negate = T)){
@@ -116,9 +121,15 @@ randomize_choice_order <- function(choice_sheet,
                     paste0(unique(choice_sheet[["list_name"]]),"_randomized"),
                     paste0(pth,unique(choice_sheet[["list_name"]]),"_randomized"))
 
-  write.csv(randomized_choices,
-            file = paste0(df_name,".csv"),
-            row.names = FALSE)
+  if (formatt == "xlsx"){
+    writexl::write_xlsx(randomized_choices,
+                        path = paste0(df_name,".xlsx"))
+  } else if (formatt == "csv"){
+    write.csv(randomized_choices,
+              file = paste0(df_name,".csv"),
+              row.names = FALSE)
+  }
+
 
 
   if(output){randomized_choices}
